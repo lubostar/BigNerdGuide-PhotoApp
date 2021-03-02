cdb1b46081f42058ae25af5bb3bfb0eaccb292da
staracek <staracek@foundation.sk>
2021-03-02 18:40:13 +0100

    implemented gallery items in RecyclerView with basic TextView in ViewHolder

--- app/src/main/java/sk/lubostar/bignerdguide/photogallery/PhotoGalleryFragment.kt
+++ app/src/main/java/sk/lubostar/bignerdguide/photogallery/PhotoGalleryFragment.kt
@@ -5,9 +5,11 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.fragment_photo_gallery.*
 
 class PhotoGalleryFragment : Fragment() {
@@ -29,6 +31,27 @@ class PhotoGalleryFragment : Fragment() {
 
         photoGalleryViewModel.galleryItemLiveData.observe(viewLifecycleOwner, { galleryItems ->
             Log.d(TAG, "Have gallery items from ViewModel $galleryItems")
+            photo_recycler_view.adapter = PhotoAdapter(galleryItems)
         })
+    }
+
+    private class PhotoHolder(itemTextView: TextView): RecyclerView.ViewHolder(itemTextView) {
+        val bindTitle: (CharSequence) -> Unit = itemTextView::setText
+    }
+
+    private class PhotoAdapter(private val galleryItems: List<GalleryItem>):
+        RecyclerView.Adapter<PhotoHolder>() {
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PhotoHolder {
+            val textView = TextView(parent.context)
+            return PhotoHolder(textView)
+        }
+
+        override fun onBindViewHolder(holder: PhotoHolder, position: Int) {
+            val galleryItem = galleryItems[position]
+            holder.bindTitle(galleryItem.title)
+        }
+
+        override fun getItemCount() = galleryItems.size
     }
 }
